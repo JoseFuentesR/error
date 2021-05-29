b8a33d1a78250a6374f97d75d0c44475559d6990
JavierWxw <javieripf11@gmail.com>
2021-05-28 22:29:46 -0400

    app funcional

--- app/src/main/java/com/example/denuncia/MainActivity.java
+++ app/src/main/java/com/example/denuncia/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.denuncia;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
+
+import com.loopj.android.http.AsyncHttpClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+
+    public boolean hay_internet(){
+        boolean conneted = false;
+        ConnectivityManager connectivityManager;
+        connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+
+        Network [] networks = connectivityManager.getAllNetworks();
+        for (Network network : networks){
+            NetworkInfo into = connectivityManager.getNetworkInfo(network);
+            if (into.isConnected()){
+                conneted = true;
+            }
+        }
+
+        return conneted;
+    }
+
+
+
+
+
 }
---
+++ app/src/main/java/com/example/denuncia/model/Encargado.java
@@ -0,0 +1,45 @@
+package com.example.denuncia.model;
+
+public class Encargado {
+
+    private int rut;
+    private String nombres_apellidos;
+    private String correo;
+    private String password;
+
+    public Encargado(){
+
+    }
+
+    public int getRut() {
+        return rut;
+    }
+
+    public void setRut(int rut) {
+        this.rut = rut;
+    }
+
+    public String getNombres_apellidos() {
+        return nombres_apellidos;
+    }
+
+    public void setNombres_apellidos(String nombres_apellidos) {
+        this.nombres_apellidos = nombres_apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
